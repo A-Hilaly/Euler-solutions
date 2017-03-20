bace3a7770bb12b271ab574c8477850a52f7f2ef
Hilaly <hilalyamine@gmail.com>
2017-03-20 21:04:52 +0100

    Add files via upload

---
+++ src/main/java/problems/EulerMain.java
@@ -0,0 +1,31 @@
+package problems;
+import problems.Euler1;
+import problems.Euler2;
+import problems.Euler3;
+import problems.Euler4;
+import problems.Euler5;
+/**
+ * Created by JAJAJAJJAJAJAJ on 20/03/17.
+ */
+public class EulerMain {
+    public static void main(String[] args)
+    {
+        Euler1 a=new Euler1();
+        System.out.println(a.solution());
+        Euler2 b=new Euler2();
+        System.out.println(b.solution());
+        Euler3 c=new Euler3();
+        System.out.println(c.solution());
+        Euler4 d=new Euler4();
+        System.out.println(d.solution());
+        Euler5 e=new Euler5();
+        System.out.println(e.solution());
+
+
+    }
+
+
+
+
+
+}
