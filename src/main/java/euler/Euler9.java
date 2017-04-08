24f5f2d6ac7aad4f9f73b640b851742c3d4d8f9b
Hilaly <hilalyamine@gmail.com>
2017-04-08 02:41:58 +0200

    Create Euler9.java

---
+++ src/main/java/euler/Euler9.java
@@ -0,0 +1,22 @@
+package euler;
+
+/**
+ * Created by JAJAJAJJAJAJAJ on 08/04/17.
+ */
+public class Euler9 {
+    public int solution(){
+        int i,j,k,p;
+        for (i=1;i<1000;i++){
+            for (j=i+1;j<1000;j++){
+
+                if (i*i+j*j==(1000-j-i)*(1000-j-i)){
+                    return i*j*(1000-j-i);
+                }
+
+            }
+        }
+    return 0;
+
+    }
+
+}
