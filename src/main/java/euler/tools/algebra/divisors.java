9e7051ad19e8f22b99c529790a7e34ded1b083b9
Hilaly <hilalyamine@gmail.com>
2017-04-10 19:20:26 +0200

    Update divisors.java

--- src/main/java/euler/tools/algebra/divisors.java
+++ src/main/java/euler/tools/algebra/divisors.java
@@ -1,8 +1,29 @@
-package tools.algebra;
-import java.euler.tools.algebra.primegenerator;
+package euler.tools.algebra;
 
-
-public class divisors(){
-    int n;
+import java.util.ArrayList;
+import java.util.Iterator;
+/**
+ * Created by JAJAJAJJAJAJAJ on 10/04/17.
+ */
+public class divisors {
     primegenerator pg = new primegenerator();
+    public boolean is_divisor(long p, long d){
+        return (p % d == 0);
+    }
+    public long number_of_divisors(long p){
+        int count = 2;
+        if (this.pg.max()< Math.sqrt(p)){
+            this.pg.GeneratePrimes((long) (Math.sqrt(p)));
+        }
+        Iterator<Long> iter = this.pg.PrimSieve.iterator();
+        while (iter.hasNext()){
+            long d = iter.next();
+            if (is_divisor(p,d)){
+                count +=2;
+            }
+        }
+        return count;
+    }
+
 }
+
