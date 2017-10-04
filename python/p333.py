3e46974cd98c611ac41b7498a3d0f458c7a304ad
a-hilaly <hilalyamine@gmail.com>
2017-10-04 10:56:28 +0200

    correct ini

--- python/p333.py
+++ python/p333.py
@@ -16,43 +16,67 @@ def _fprimes(s,e):
             l.append(i)
     return l
 
-_p = _fprimes(2, 1000000)
 
-primes = dict(zip(_p, [0]*len(_p)))
+class P333Solver(object):
 
-print("got primes :", len(_p), _p[:20])
+    def __init__(self):
+        self._init_env()
 
-L = 1000000
-powers = {2: [1], 3: [1]}
+    def _init_env(self):
+        self.L = 1000000
+        self.primes = self._init_primes()[1]
+        self._init_powers()
+        self.pmatrix = None
 
-for e in [2, 3]:
-    while powers[e][-1] < 1000000:
-        powers[e].append(powers[e][-1]*e)
-
-p = [[powers[3][i3]*powers[2][i2] for i3 in range(len(powers[3]))]
-     for i2 in range(len(powers[2]))]
-
-print("got powers :", len(powers), len(powers[2]), len(powers[3]))
+    def _init_primes(self):
+        _ = _fprimes(2, self.L)
+        return _, dict(zip(_, [0] * len(_)))
 
 
-def rec(x, start_y, mysum):
-    if x == -1:
-        if mysum in primes:
-            primes[mysum] += 1
-    else:
-        rec(x-1, start_y, mysum)
-        for y in range(start_y, len(powers[2])):
-            s = mysum + p[y][x]
-            if s >= L:
-                return
-            rec(x-1, y+1, s)
-    return
+    def _init_powers(self):
+        self.powers = {2: [1], 3: [1]}
+        for e in [2, 3]:
+            while self.powers[e][-1] < self.L:
+                self.powers[e].append(self.powers[e][-1]*e)
+        self.pmatrix = [
+            [self.powers[3][i3] * self.powers[2][i2] for i3 in range(
+                len(self.powers[3]))
+            ] for i2 in range(
+                len(self.powers[2])
+            )
+        ]
+        print(self.pmatrix)
 
-rec(len(powers[3])-1, 0, 0)
+    def rec(self, x, sy, summ):
+        if x == -1:
+            if summ in self.primes:
+                self.primes[summ] += 1
+        else:
+            self.rec(x - 1, sy, summ)
+            for y in range(sy, len(self.powers[2])):
+                print(x, y, sy, summ)
+                s = summ + self.pmatrix[y][x]
+                if s >= self.L:
+                    return
+                self.rec(x - 1, y + 1, s)
+        return
 
-mysum = 0
-for p, count in primes.items():
-    if count == 1:
-        mysum += p
+    def _solve(self):
+        try:
+            self.rec(len(self.powers[3])-1, 0, 0)
+        except:
+            raise Exception("Fix me")
+        summ = 0
+        for p, count in self.primes.items():
+            if count == 1:
+                summ += p
+        return summ
 
-print("mysum" , mysum)
+    @classmethod
+    def solve(cls):
+        obj = object.__new__(cls)
+        obj.__init__()
+        print(obj._solve())
+
+if __name__ == "__main__":
+    P333Solver.solve()
